d93a72d8dfa996909bc57fe082f9f7d278e4251b
samuelvnimwegen <samuelvannimwegen@gmail.com>
2023-10-01 16:34:41 +0200

    setup

---
+++ CFG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Samuel on 01/10/2023.
+//
+
+#include "CFG.h"
---
+++ CFG.h
@@ -0,0 +1,20 @@
+//
+// Created by Samuel on 01/10/2023.
+//
+
+#ifndef MB_CFG_H
+#define MB_CFG_H
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class CFG {
+    vector<string> nonTerminals;
+    vector<string> terminals;
+
+public:
+
+};
+
+
+#endif //MB_CFG_H
